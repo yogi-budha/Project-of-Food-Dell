71da5f3c5e3618d9038db8149eb74bc04175b2ab
yogi-budha <143183699+yogi-budha@users.noreply.github.com>
2024-09-30 06:57:41 +0545

    navbar home change

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = ({ setShowLogin }) => {
           }}
           className={menu == 'Home' ? 'active' : ''}
         >
-          home
+          Home
         </Link>
         {/* <a
           href="#explore-menu"
